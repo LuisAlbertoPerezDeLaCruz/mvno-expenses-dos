a59cf3464a91bbc489a0d1fff18e920c1cdba1b8
Luis Alberto Perez <luisalbertoperez@gmail.com>
2026-03-04 03:56:09 -0400

    Incluida pagina employees

---
+++ app/employees/page.tsx
@@ -0,0 +1,12 @@
+export default function Employees() {
+  return (
+    <div>
+      {/* Contenido principal */}
+      <main className="max-w-7xl mx-auto px-4 py-10">
+        <h2 className="text-2xl font-bold text-gray-900">
+          Listado de empleados
+        </h2>
+      </main>
+    </div>
+  );
+}
